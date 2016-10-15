900fd0e84e1156303d5d94aa9282454e4b04e52f
SrinivasaRaoMakkena <makkenasrinivasarao1@gmail.com>
2016-10-15 16:39:02 -0500

    Initial commit

---
+++ app/src/main/java/com/nwmissouri/emrgencyreport/Register.java
@@ -0,0 +1,19 @@
+package com.nwmissouri.emrgencyreport;
+
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/nwmissouri/emrgencyreport/SampleScreen.java
@@ -0,0 +1,13 @@
+package com.nwmissouri.emrgencyreport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_screen);
+    }
+}
